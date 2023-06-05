94d3d62460ad39614f1e0602652536291037ebaf
zelenkovaalex <sashazelenkova39@gmail.com>
2023-06-05 22:28:07 +0300

    o

--- javascript/animation.js
+++ javascript/animation.js
@@ -1,3 +1,31 @@
 $(document).ready(function() {
 
-}
+// timetable
+
+
+
+// form
+
+$(".butt").click(function() {
+  $(".thanks").removeClass("none");
+});
+
+$(".tin1").draggable(function () {
+});
+
+$(".tin1").droppable(function () {
+  $('this').css('transform', 'opacity(0)');
+});
+
+// merch
+
+$(".order").click(function() {
+  $(".process").removeClass("none");
+});
+
+// afishi
+
+
+
+
+})
