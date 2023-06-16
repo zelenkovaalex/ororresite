6a3ba28533d53556ebf9bc5eb0700f5e80fe6640
zelenkovaalex <sashazelenkova39@gmail.com>
2023-06-16 15:26:08 +0300

    щщщщщ

--- javascript/animation.js
+++ javascript/animation.js
@@ -43,6 +43,115 @@ $(".order").click(function() {
   $(".process").toggleClass("none");
 });
 
+$(".shopping_card1").click(function() {
+  $(".done1").removeClass("none");
+  $(".shopping_card1").addClass("none");
+});
+$(".done1").click(function() {
+  $(".shopping_card1").removeClass("none");
+  $(".done1").addClass("none");
+});
+
+$(".shopping_card2").click(function() {
+  $(".done2").removeClass("none");
+  $(".shopping_card2").addClass("none");
+});
+$(".done2").click(function() {
+  $(".shopping_card2").removeClass("none");
+  $(".done2").addClass("none");
+});
+
+$(".shopping_card3").click(function() {
+  $(".done3").removeClass("none");
+  $(".shopping_card3").addClass("none");
+});
+$(".done3").click(function() {
+  $(".shopping_card3").removeClass("none");
+  $(".done3").addClass("none");
+});
+
+$(".shopping_card4").click(function() {
+  $(".done4").removeClass("none");
+  $(".shopping_card4").addClass("none");
+});
+$(".done4").click(function() {
+  $(".shopping_card4").removeClass("none");
+  $(".done4").addClass("none");
+});
+
+$(".shopping_card5").click(function() {
+  $(".done5").removeClass("none");
+  $(".shopping_card5").addClass("none");
+});
+$(".done5").click(function() {
+  $(".shopping_card5").removeClass("none");
+  $(".done5").addClass("none");
+});
+
+$(".shopping_card6").click(function() {
+  $(".done6").removeClass("none");
+  $(".shopping_card6").addClass("none");
+});
+$(".done6").click(function() {
+  $(".shopping_card6").removeClass("none");
+  $(".done6").addClass("none");
+});
+
+$(".shopping_card7").click(function() {
+  $(".done7").removeClass("none");
+  $(".shopping_card7").addClass("none");
+});
+$(".done7").click(function() {
+  $(".shopping_card7").removeClass("none");
+  $(".done7").addClass("none");
+});
+
+$(".shopping_card8").click(function() {
+  $(".done8").removeClass("none");
+  $(".shopping_card8").addClass("none");
+});
+$(".done8").click(function() {
+  $(".shopping_card8").removeClass("none");
+  $(".done8").addClass("none");
+});
+
+$(".shopping_card9").click(function() {
+  $(".done9").removeClass("none");
+  $(".shopping_card9").addClass("none");
+});
+$(".done9").click(function() {
+  $(".shopping_card9").removeClass("none");
+  $(".done9").addClass("none");
+});
+
+$(".shopping_card10").click(function() {
+  $(".done10").removeClass("none");
+  $(".shopping_card10").addClass("none");
+});
+$(".done10").click(function() {
+  $(".shopping_card10").removeClass("none");
+  $(".done10").addClass("none");
+});
+
+$(".shopping_card11").click(function() {
+  $(".done11").removeClass("none");
+  $(".shopping_card11").addClass("none");
+});
+$(".done11").click(function() {
+  $(".shopping_card11").removeClass("none");
+  $(".done11").addClass("none");
+});
+
+$(".shopping_card12").click(function() {
+  $(".done12").removeClass("none");
+  $(".shopping_card12").addClass("none");
+});
+$(".done12").click(function() {
+  $(".shopping_card12").removeClass("none");
+  $(".done12").addClass("none");
+});
+
+
 // afishi
 
 
